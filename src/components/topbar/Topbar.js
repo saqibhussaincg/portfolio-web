5db3142143899bf8913e907b4a530c49193aded7
Saqib Hussain - Designer + Coder <saqibhussaincg@gmail.com>
2022-03-28 22:17:00 +0500

    'nav-smooth-scroll'

---
+++ src/components/topbar/Topbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './topbar.scss'
+
+function Topbar() {
+  return (
+    <div className='topbar'>
+      <div className='warapper'>
+        <div className='left'>
+          <a href='#intro'>asdaddas </a>
+        </div>
+        <div className='right'>
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Topbar
---
+++ src/components/work/Work.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './work.scss'
+
+function Work() {
+  return (
+    <div className='works' id='work'>Work</div>
+  )
+}
+
+export default Work
