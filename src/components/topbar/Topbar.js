6f3d047d26c0512eaab4e0bebff4c4c5a4164fc3
Saqib Hussain - Designer + Coder <saqibhussaincg@gmail.com>
2022-03-28 23:59:05 +0500

    'navigation'

--- src/components/topbar/Topbar.js
+++ src/components/topbar/Topbar.js
@@ -1,15 +1,27 @@
 import React from 'react'
 import './topbar.scss'
+import EmailIcon from '@mui/icons-material/Email';
+import PersonIcon from '@mui/icons-material/Person';
 
 function Topbar() {
   return (
     <div className='topbar'>
-      <div className='warapper'>
+      <div className='wrapper'>
         <div className='left'>
-          <a href='#intro'>asdaddas </a>
+          <a href='#intro' className='logo'> genius. </a>
+
+          <div className='itemContainer' >
+            <PersonIcon className='icon' />
+            <span>+92 123 456 789</span>
+          </div>
+
+          <div className='itemContainer'>
+            <EmailIcon className='icon' />
+            <span>saqib@genius.com</span>
+          </div>
+
         </div>
         <div className='right'>
-
         </div>
       </div>
     </div>
